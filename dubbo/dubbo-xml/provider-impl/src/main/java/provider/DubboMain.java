9d4a00747be75553806a238f402d646a8ee946fc
xiaoluo <luo_tian_fu@163.com>
2019-07-29 11:17:48 +0800

    xml配置方式暴露和调用服务

---
+++ dubbo/dubbo-xml/provider-impl/src/main/java/provider/DubboMain.java
@@ -0,0 +1,16 @@
+package provider;
+
+import org.apache.dubbo.container.Main;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * dubbo 容器启动
+ * 实际也是封装了spring容器的启动，会自动加载 META-INF/spring/*.xml文件
+ */
+public class DubboMain {
+    public static void main(String[] args) throws InterruptedException {
+        Main.main(args);
+    }
+}
--- dubbo/dubbo-xml/provider-impl/src/main/java/provider/ProviderMain.java
+++ dubbo/dubbo-xml/provider-impl/src/main/java/provider/ProviderMain.java
@@ -1,9 +1,18 @@
 package provider;
 
-import org.apache.dubbo.container.Main;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * spring 容器启动
+ */
 public class ProviderMain {
-    public static void main(String[] args) {
-        Main.main(args);
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:provider.xml");
+        context.start();
+        latch.await();
+
     }
 }
