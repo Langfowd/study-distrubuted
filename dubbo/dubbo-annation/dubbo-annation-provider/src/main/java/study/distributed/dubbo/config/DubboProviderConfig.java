7a192a2a9507ae509e5719a9c4f4b279acb0f4b9
xiaoluo <luo_tian_fu@163.com>
2019-07-31 10:07:48 +0800

    修改包扫描配置

--- dubbo/dubbo-annation/dubbo-annation-provider/src/main/java/study/distributed/dubbo/config/DubboProviderConfig.java
+++ dubbo/dubbo-annation/dubbo-annation-provider/src/main/java/study/distributed/dubbo/config/DubboProviderConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@EnableDubbo(scanBasePackages = "study.distributed.META-INF")
+@EnableDubbo(scanBasePackages = "study.distributed.dubbo")
 @PropertySource("classpath:/provider.properties")
 public class DubboProviderConfig {
 }
